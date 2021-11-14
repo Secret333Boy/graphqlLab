4107b3d20a87f14b841b8e9556b1c9a2f22928ee
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-11-14 09:13:21 +0200

    Simple auth implemented

---
+++ api/auth.js
@@ -0,0 +1,55 @@
+const crypto = require('crypto');
+const { createClient } = require('urql');
+require('isomorphic-unfetch');
+const client = createClient({
+  url: 'https://' + process.env.graphQLPath,
+  fetchOptions: {
+    headers: {
+      'x-hasura-admin-secret': process.env.hasuraAdminSecret,
+    },
+  },
+});
+
+const insertUserMutationTemplate = `mutation insertUser {
+  insert_user_one(object: {hash: "$hash", login: "$login"}) {
+    id
+  }
+}`;
+
+const getUserByHashTemplate = `query getUserByHash {
+  user(where: {_and: {hash: {_eq: "$hash"}, login: {_eq: "$login"}}}) {
+    id
+    hash
+    login
+  }
+}`;
+
+module.exports = async (req, res) => {
+  const login = req.headers.login;
+  const pass = req.headers.pass;
+  const salt =
+    login
+      .split('')
+      .map((el, i) => String.fromCharCode(el.charCodeAt(0) + i))
+      .join('|') + (process.env.SALT_MODIFICATION || '');
+  const code = crypto
+    .createHash('sha256')
+    .update(login + salt + pass)
+    .digest('base64');
+
+  const getUserByHash = getUserByHashTemplate
+    .replace('$hash', code)
+    .replace('$login', login);
+  const { data } = await client.query(getUserByHash).toPromise();
+  if (data.user.length > 0) {
+    res.status(200).json(code);
+  }
+
+  const insertUserMutation = insertUserMutationTemplate
+    .replace('$hash', code)
+    .replace('$login', login);
+  const { error } = await client.mutation(insertUserMutation).toPromise();
+  if (error) {
+    res.status(500).json(error);
+  }
+};
--- src/AuthOnly/AuthOnly.jsx
+++ src/AuthOnly/AuthOnly.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './AuthOnly.css';
 
-const doAuth = () => {
-  //doThis
-};
-
 const AuthOnly = ({ children }) => {
-  if (!document.cookie) {
+  const loginRef = useRef(null);
+  const passRef = useRef(null);
+  const doAuth = async (e) => {
+    e.preventDefault();
+    const login = loginRef.current.value;
+    const pass = passRef.current.value;
+
+    const res = await fetch('/api/auth', {
+      headers: { login, pass },
+    });
+
+    document.cookie = 'token=' + (await res.json());
+    document.location.reload();
+  };
+
+  const token = document.cookie
+    .split(';')
+    .filter((item) => item.startsWith('token='))
+    .join('')
+    .replace('token=', '');
+  if (!token) {
     return (
       <form className="auth">
-        <input type="text" placeholder="Login" />
-        <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Login" ref={loginRef} />
+        <input type="password" placeholder="Password" ref={passRef} />
         <input type="submit" onClick={doAuth} />
       </form>
     );
